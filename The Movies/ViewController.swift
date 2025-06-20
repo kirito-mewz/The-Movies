d7f155c49b8853bda837eb67149704e6ad12c10d
Kirito <mghtetpai.1999@gmail.com>
2025-06-20 18:16:53 +0700

    Initial Commit

---
+++ The Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movies
+//
+//  Created by PaingHtet on 20/06/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
