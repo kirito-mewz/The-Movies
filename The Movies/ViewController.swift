e32c2ed168ecd31d97b3643cfc31fead29822886
kirito <mghtetpai.1999@gmail.com>
2025-08-01 13:45:52 +0700

    Initial Commit

---
+++ The Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movies
+//
+//  Created by Paing Htet on 01/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ The MoviesTests/The_MoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  The_MoviesTests.swift
+//  The MoviesTests
+//
+//  Created by Paing Htet on 01/08/2025.
+//
+
+import Testing
+@testable import The_Movies
+
+struct The_MoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
